bc41693e10e6495fd135170fdb2caf400477e414
Antonio D'Onchia <donchiadeveloper@gmail.com>
2017-05-25 08:35:39 +0200

    Initial Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,42 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todo').insertOne( {
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, res) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err)
+    //     }
+
+    //     console.log(JSON.stringify(res.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').find({location: 'Palagiano'}).count( (err, count) => {
+    //     if (err) {
+    //         return console.log('ERROR: ', err)
+    //     }
+    //     console.log('Numero di Documenti: ', count);
+    // })
+
+    db.collection('Users').find({location: 'Palagiano'}).toArray( (err, documents) => {
+        if (err) {
+            return console.log('ERROR: ', err)
+        }
+        console.log(JSON.stringify(documents, undefined, 2));
+    })
+
+    //db.close();
+
+})
+
+
+
